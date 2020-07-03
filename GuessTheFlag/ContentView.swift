6ca4be13f895fb123be4dbdcbaf17ae9a6cb2abb
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-04 02:34:18 +0300

    Update ContentView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
-
+    
     var body: some View {
-
+        
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
@@ -49,25 +49,26 @@ struct ContentView: View {
                         self.askQuestion()
                     })
         }
+        
         //        LinearGradient(gradient: Gradient(colors: [Color.red, Color(red: 1, green: 0.8, blue: 0)]), startPoint: .leading, endPoint: .trailing)
         //        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 20, endRadius: 200)
         //        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
-
+    
     func flagTapped(_ number: Int) {
-
+        
         if number == correctAnswer {
             score += 1
             scoreTitle = "Correct"
         } else {
             scoreTitle = "Wrong"
         }
-
+        
         showingScore = true
     }
-
+    
     func askQuestion() {
-
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
