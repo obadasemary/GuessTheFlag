7886fae98658ec56b0ab876726bc45ba438dc27e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-04 02:12:26 +0300

    Showing the player’s score with an alert
    Showing the player’s score with an alert – Guess the Flag SwiftUI Tutorial 7/8
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
 
-//    @State private
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-
-    var correctAnswer = Int.random(in: 0...2)
-
-//    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
 
     var body: some View {
 
@@ -29,11 +27,9 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .bold()
                 }
-
                 ForEach(0 ..< 3) { number in
-
                     Button(action: {
-                        //                self.showingAlert = true
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -41,6 +37,12 @@ struct ContentView: View {
                 }
                 Spacer()
             }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton:
+                    .default(Text("Continue")) {
+                        self.askQuestion()
+                    })
         }
 
 //        Button(action: {
@@ -72,6 +74,23 @@ struct ContentView: View {
 //        LinearGradient(gradient: Gradient(colors: [Color.red, Color(red: 1, green: 0.8, blue: 0)]), startPoint: .leading, endPoint: .trailing)
 //        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 20, endRadius: 200)
 //        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+
+    func flagTapped(_ number: Int) {
+
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+
+    func askQuestion() {
+
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
