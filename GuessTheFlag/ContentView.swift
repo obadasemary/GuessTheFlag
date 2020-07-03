054ae256ce42c0f7be62d5904c376a7d8e5d1023
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-04 02:46:09 +0300

    feat: Finalize Guess the flag
    Guess the Flag SwiftUI

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,69 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
-    @State private var score = 0
-    
     var body: some View {
-        
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .bold()
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                }
-                ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/))
-                            .shadow(color: .black, radius: 2)
-                    }
-                }
-                Spacer()
-            }
-        }
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton:
-                    .default(Text("Continue")) {
-                        self.askQuestion()
-                    })
-        }
-        
-        //        LinearGradient(gradient: Gradient(colors: [Color.red, Color(red: 1, green: 0.8, blue: 0)]), startPoint: .leading, endPoint: .trailing)
-        //        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 20, endRadius: 200)
-        //        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-    }
-    
-    func flagTapped(_ number: Int) {
-        
-        if number == correctAnswer {
-            score += 1
-            scoreTitle = "Correct"
-        } else {
-            scoreTitle = "Wrong"
-        }
-        
-        showingScore = true
-    }
-    
-    func askQuestion() {
-        
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        Text("Hello, world!").padding()
     }
 }
 
