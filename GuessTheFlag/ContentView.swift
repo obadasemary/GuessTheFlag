ca23e4feb0880251d5682634ae35f6fef71dee4e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-04 00:15:29 +0300

    Gradients
    Gradients – Guess the Flag SwiftUI Tutorial 3/8
    
    LinearGradient
    RadialGradient
    AngularGradient
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,19 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-//            Color(red: 1, green: 0.8, blue: 0)
-//            Color.red.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Hello, world!")
-                Text("Hello, world!. I am here 🚀😎")
-            }
-            HStack(spacing: 20) {
-                Text("Hello, world!")
-                Text("Hello, world!. I am back 🚀😎")
-            }
-        }
-        .background(Color(red: 1, green: 0.8, blue: 0))
+//        ZStack {
+////            Color(red: 1, green: 0.8, blue: 0)
+////            Color.red.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+//            VStack(alignment: .leading, spacing: 5) {
+//                Text("Hello, world!")
+//                Text("Hello, world!. I am here 🚀😎")
+//            }
+//            HStack(spacing: 20) {
+//                Text("Hello, world!")
+//                Text("Hello, world!. I am back 🚀😎")
+//            }
+//        }
+//        .background(Color(red: 1, green: 0.8, blue: 0))
+//        LinearGradient(gradient: Gradient(colors: [Color.red, Color(red: 1, green: 0.8, blue: 0)]), startPoint: .leading, endPoint: .trailing)
+//        RadialGradient(gradient: Gradient(colors: [Color.blue, Color.black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 20, endRadius: 200)
+        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
 
